8e64b93bcfa7cafd3669907c16fda8cb449ebc82
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-05-06 18:00:05 +0100

    Refactor projects scss code

--- app/src/components/Project.jsx
+++ app/src/components/Project.jsx
@@ -11,17 +11,22 @@ function Project(props) {
     
   return (
     <div className="projects-items-box">
-        <div className="projects-items-box-details"></div>
+      <div className="projects-items-box-details"></div>
+
       <div className="projects-items-box-img">
-      <div className="projects-items-box-details-main">
-            <h3>{props.title}</h3>
-            {categoryElements}
+
+        <div className="projects-items-box-details-main">
+          <h3>{props.title}</h3>
+          {categoryElements}
         </div>  
+
         <p>{props.description}</p>
+
         <div className="projects-items-box-details-links">
             <a href={props.siteLink} target="_blank" className="projects-items-box-details-links-main">Visit Site</a>
             <a href={props.githubLink} className="projects-items-box-details-links-reverse" target="_blank">View Code</a>
         </div>
+        
       </div>
     </div>
   );
