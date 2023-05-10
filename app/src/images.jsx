1e083de7689dd1f29d1bbb572de4b74b3b13e134
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-05-10 18:45:41 +0100

    Update images.jsx

--- app/src/images.jsx
+++ app/src/images.jsx
@@ -1,7 +1,20 @@
 import sassImg from '../src/images/sass-learn-sass-course.png';
 import jsFundaImg from '../src/images/js-learn-js-fundamentals-course.png';
+import learnColorDesign from '../src/images/learn-color-design-course.png';
+import flexboxAndGrid from '../src/images/learn-css-flexbox-and-grid-course.png';
+import cssIntro from '../src/images/learn-css-intro-course.png';
+import htmlForms from '../src/images/learn-html-forms-course.png';
+import cssColorDesign from '../src/images/learn-color-design-course.png';
+import cssBoxModel from '../src/images/learn-css-box-model-and-layout-course.png';
 
 export default {
     sassImg,
-    jsFundaImg
+    jsFundaImg,
+    learnColorDesign,
+    flexboxAndGrid,
+    cssIntro,
+    cssTypographyAndFonts,
+    htmlForms,
+    cssColorDesign,
+    cssBoxModel
 }
