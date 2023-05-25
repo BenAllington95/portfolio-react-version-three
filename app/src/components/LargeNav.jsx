5a43fc0d68488fed3dfc6cb1e27c7bc657d0f18e
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-05-25 18:44:47 +0100

    Add colour to large nav when scroll down

--- app/src/components/LargeNav.jsx
+++ app/src/components/LargeNav.jsx
@@ -1,63 +1,56 @@
-
 import React, { useEffect , useState } from 'react'
 import { Link } from "react-scroll";
 
 export default function LargeNav(props) {
     const [prevScrollPos, setPrevScrollPos] = useState(0);
     const [visible, setVisible] = useState(true);
+    const [scrolled, setScrolled] = useState(false); // State variable to keep track of whether user has scrolled
 
-const navItems = [
-      {id:"#hero-section", text: "Home"},
-      {id:"#about-section", text: "About"},
-      {id:"#certificates-section", text: "Certificates"},
-      {id:"#projects-section", text: "Projects"},
-      {id: null, toggleTheme: true, text: (<ion-icon name={props.isDarkMode ? "moon" : "sunny"}></ion-icon>)},
-  ]
+    const navItems = [
+        {id:"#hero-section", text: "Home"},
+        {id:"#about-section", text: "About"},
+        {id:"#certificates-section", text: "Certificates"},
+        {id:"#projects-section", text: "Projects"},
+        {id: null, toggleTheme: true, text: (<ion-icon name={props.isDarkMode ? "moon" : "sunny"}></ion-icon>)},
+    ]
 
-useEffect(() => {
-    
-    const handleScroll = () => {
-      const currentScrollPos = window.pageYOffset;
-      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
-      setPrevScrollPos(currentScrollPos);
-    };
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollPos = window.pageYOffset;
+            setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+            setPrevScrollPos(currentScrollPos);
+            setScrolled(currentScrollPos > 300); // If scroll position is greater than 0, user has scrolled
+        };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-    
-  }, [prevScrollPos, visible]);
-  
-  // const navElements = navItems.map(item => {
-  //     return (
-  //       <a key={item.id} href={item.id} onClick={item.toggleTheme ? props.handleToggleTheme : null}>
-  //           <li>{item.text}</li>
-  //       </a>
-  //     )})
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, [prevScrollPos, visible]);
 
-  const navElements = navItems.map(item => {
-    if (item.toggleTheme) {
-      return (
-        <a key={item.id} onClick={props.handleToggleTheme}>
-          <li>{item.text}</li>
-        </a>
-      );
-    }
-  
+    const navElements = navItems.map(item => {
+        if (item.toggleTheme) {
+            return (
+                <a key={item.id} onClick={props.handleToggleTheme}>
+                    <li>{item.text}</li>
+                </a>
+            );
+        }
+
+        return (
+            <Link
+                key={item.id}
+                to={item.id.substring(1)}
+                smooth={true}
+                duration={1000}
+                offset={0.1}
+            >
+                <li>{item.text}</li>
+            </Link>
+        );
+    });
+
+
     return (
-      <Link
-        key={item.id}
-        to={item.id.substring(1)}
-        smooth={true}
-        duration={1000}
-        offset={0.1} // Adjust this value to account for fixed headers or other offsets
-      >
-        <li>{item.text}</li>
-      </Link>
-    );
-  });
-    
-    return (
-        <div className={`large-nav ${!visible ? "hidden" : ""}`}>
+        <div className={`large-nav ${!visible ? "hidden" : ""} ${scrolled ? "scrolled" : ""}`}>
             <ul>
                 {navElements}
             </ul>
